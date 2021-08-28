8de902ecffc13a8bcea90945f4ad81afafb7060b
Prakash Goud <prakash.goud@morningstar.com>
2021-08-28 11:53:27 +0530

    [Prakash] - Created the feture for user to choose letter X or O in game

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,13 +1,32 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.*;
+
 public class TicTacToeGame {
 
 	private Character[] gameBoard = new Character[10];
+	Scanner sc = new Scanner(System.in);
+	public char user;
+	public char computer;
 
-	public void createBorad() {
-		for(int i=1;i<10;i++) {
+	public void createBoard() {
+		for (int i = 1; i < 10; i++) {
 			gameBoard[i] = ' ';
 		}
+	}
+
+	public char chooseLetter() {
+		System.out.println("1. X");
+		System.out.println("2. O");
+		System.out.println("Enter case number to choose letter:- ");
+		int result = sc.nextInt();
+		if (result == 1) {
+			user = 'X';
+		} else {
+			computer = 'O';
+		}
+		return user;
+
 	}
 
 }
--- src/com/bridgelabz/tictactoe/TicTacToeRunner.java
+++ src/com/bridgelabz/tictactoe/TicTacToeRunner.java
@@ -6,7 +6,15 @@ public class TicTacToeRunner {
 		
 		System.out.println("Welcome to tic tac toe game");
 		TicTacToeGame tictactoe = new TicTacToeGame();
-		tictactoe.createBorad();
+		tictactoe.createBoard();
+		char choiceOfUser = tictactoe.chooseLetter();
+		if(choiceOfUser == 'X') {
+			System.out.println("you have chose letter X and computer has got letter O");
+		}
+		else {
+			System.out.println("you have chose letter O and computer has got letter X");
+		}
+		
 
 	}
 
